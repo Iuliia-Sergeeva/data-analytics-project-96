f3b2d6b49a046a88917dbffe798adceeb6a9a5bf
Юлия <144040954+Iuliia-Sergeeva@users.noreply.github.com>
2023-11-03 13:17:24 +0300

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,39 +1,46 @@
 --Шаг 2. Сценарий атрибуции
 --запрос для атрибуции лидов по модели Last Paid Click (топ-10 записей)
-with Last_Paid_Click as(
-select
-	distinct s.visitor_id,
-	s.visit_date,
-	s.source as utm_source,
-	s.medium as utm_medium,
-	s.campaign as utm_campaign,
-	l.lead_id,
-	l.created_at as created_at,
-	l.amount,
-	l.closing_reason,
-	l.status_id,
-	row_number () over (partition by l.lead_id order by l.created_at desc) as rn
+with Last_Paid_Click as (
+    select
+        s.visitor_id,
+        s.visit_date,
+        s.source as utm_source,
+        s.medium as utm_medium,
+        s.campaign as utm_campaign,
+        case
+            when l.created_at < visit_date then null else l.lead_id
+            end as lead_id,
+        case
+            when l.created_at < visit_date then null else l.created_at
+            end as created_at,
+        l.amount,
+        l.closing_reason,
+        l.status_id,
+        row_number()
+            over (partition by l.lead_id order by s.visit_date desc) as rn
 from sessions s
-left join leads l on l.visitor_id = s.visitor_id
+left join leads l
+on l.visitor_id = s.visitor_id and l.created_at >= s.visit_date 
 where medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
-order by
-	amount desc nulls last,
-	visit_date asc nulls last,
-	s.source asc nulls last,
-	s.medium asc nulls last,
-	s.campaign asc nulls last
 )
-select 
-	visitor_id,
-	visit_date,
-	utm_source,
-	utm_medium,
-	utm_campaign,
-	lead_id,
-	created_at,
-	amount,
-	closing_reason,
-	status_id
+
+select
+    visitor_id,
+    visit_date,
+    utm_source,
+    utm_medium,
+    utm_campaign,
+    lead_id,
+    created_at,
+    amount,
+    closing_reason,
+    status_id
 from Last_Paid_Click
 where rn = 1
+order by
+    amount desc nulls last,
+    visit_date asc nulls last,
+    utm_source asc nulls last,
+    utm_medium asc nulls last,
+    utm_campaign asc nulls last
 limit 10;
