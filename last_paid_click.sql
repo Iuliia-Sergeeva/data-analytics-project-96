2ed7625de08d8c4950a0ae7838d64b3a023f62c9
Iuliia-Sergeeva <sergeeva_yul@bk.ru>
2023-11-22 14:03:41 +0300

    Изменение кода для шага 2.

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -7,23 +7,19 @@ with Last_Paid_Click as (
         s.source as utm_source,
         s.medium as utm_medium,
         s.campaign as utm_campaign,
-        case
-            when l.created_at < visit_date then null else l.lead_id
-            end as lead_id,
-        case
-            when l.created_at < visit_date then null else l.created_at
-            end as created_at,
+        l.lead_id,
+        l.created_at,
         l.amount,
         l.closing_reason,
         l.status_id,
         row_number()
-            over (partition by l.lead_id order by s.visit_date desc) as rn
+            over (partition by s.visitor_id order by s.visit_date desc) as rn
 from sessions s
 left join leads l
-on l.visitor_id = s.visitor_id and l.created_at >= s.visit_date 
+on l.visitor_id = s.visitor_id
+and l.created_at >= s.visit_date 
 where medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
 )
-
 select
     visitor_id,
     visit_date,
